6f9f103eb016e58f10e5643a249dccf2aeae7239
VlaDOS <45236519+teyhd@users.noreply.github.com>
2022-09-16 12:00:14 +0300

    port

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ function getadmin(str){
   return 1
 }
 
-var PORT = process.env.PORT || 80; 
+var PORT = process.env.PORT || 501; 
 async function start(){
     try {
         app.listen(PORT,()=> {
